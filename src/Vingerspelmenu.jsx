b3d81f0a60a31ca682f6c9e328fdeb2ea048d7c2
justin <kinhanjustin@gmail.com>
2025-03-05 14:01:04 +0100

    Vingerspel overzicht toegevoegd

--- src/Vingerspelmenu.jsx
+++ src/Vingerspelmenu.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
 function Vingerspelmenu() {
-  return (
-    <div>
-      <h1>Vingerspelmenu</h1>
-      <p>Welkom bij het vingerspelmenu</p>
-    </div>
-  );
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    return (
+        <div className="p-9 text-left">
+            <h1 className="text-3xl font-bold my-2 ml-[5%]">Vingerspel</h1>
+            <p className="text-base my-2 ml-[5%]">Leer hier alle letters in gebarentaal</p>
+            <div className="flex justify-center mt-[10%]">
+                <button className="w-72 px-4 py-3 text-2xl font-bold cursor-pointer bg-[#CF0245] text-white rounded-lg">Leren</button>
+            </div>
+            <div className="flex justify-center mt-8">
+                <div className="grid grid-cols-3 gap-8">
+                    {alphabet.map((letter) => (
+                        <div key={letter}
+                             className="flex flex-col items-center justify-center w-20 h-24 border-2 border-[#CF0245] bg-white rounded-lg font-bold text-2xl pb-3">
+                            {letter}
+                            <div className="w-3/4 h-2 bg-[#D9D9D9] mt-[20%] rounded"></div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Vingerspelmenu;
