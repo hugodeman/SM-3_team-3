0672012422cc307270984dda0f34538449b9bb08
wesle <wesleynederpel43@gmail.com>
2025-03-10 14:23:16 +0100

    Simplify code DELETE Button component

--- src/Lesstof.jsx
+++ src/Lesstof.jsx
@@ -1,46 +1,33 @@
-function Les(){
+import NavbarMobile from "./components/navbar-mobile.jsx";
 
-    return(
+function Les() {
+    const weeks = [
+        "Week 1", "Week 2", "Week 3", "Week 4",
+        "Week 5", "Week 6", "Week 7", "Week 8"
+    ];
+
+    return (
         <>
             <div>
                 <h1>Les stof</h1>
             </div>
-            <div className={'pb-10 mb-10'}>
-                <div className={'flex justify-around mb-6'}>
-                    <div>
-                        <button className="bg-[#A90D3D] py-4 px-10 text-white rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl ">Week 1</button>
+            <div className="pb-10 mb-10">
+                {/* Map through weeks and render buttons in pairs */}
+                {Array.from({ length: weeks.length / 2 }).map((_, i) => (
+                    <div key={i} className="flex justify-around mb-6">
+                        {weeks.slice(i * 2, i * 2 + 2).map((week, j) => (
+                            <div key={j}>
+                                <button className="bg-[#A90D3D] py-4 px-10 text-white rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl">
+                                    {week}
+                                </button>
+                            </div>
+                        ))}
                     </div>
-                    <div>
-                        <button className="bg-[#A90D3D] py-4 px-10 text-white rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl">Week 2</button>
-                    </div>
-                </div>
-                <div className={'flex justify-around mb-6'}>
-                    <div>
-                        <button className="bg-[#A90D3D] py-4 px-10 text-white rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl">Week 3</button>
-                    </div>
-                    <div>
-                        <button className="bg-[#A90D3D] py-4 px-10 text-white rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl">Week 4</button>
-                    </div>
-                </div>
-                <div className={'flex justify-around mb-6'}>
-                <div>
-                        <button className="bg-[#A90D3D] py-4 px-10 text-white rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl">Week 5</button>
-                    </div>
-                    <div>
-                        <button className="bg-[#A90D3D] py-4 px-10 text-white rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl">Week 6</button>
-                    </div>
-                </div>
-                <div className={'flex justify-around'}>
-                    <div>
-                        <button className="bg-[#A90D3D] py-4 px-10 text-white rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl">Week 7</button>
-                    </div>
-                    <div>
-                        <button className="bg-[#A90D3D] py-4 px-10 text-white rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl">Week 8</button>
-                    </div>
-                </div>
+                ))}
             </div>
+            <NavbarMobile />
         </>
-    )
+    );
 }
 
-export default Les
+export default Les;
--- src/components/button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = () => {
-    return (
-        <div>
-            <button className="bg-[#A90D3D] py-4 px-10 text-white rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl">Text</button>
-        </div>
-    )
-}
-
-export default Button;
