3ee2dc49c0597feb404f19b469ef66c15537ee1d
justin <kinhanjustin@gmail.com>
2025-03-11 11:23:17 +0100

    Margin fix

--- src/Vingerspelmenu.jsx
+++ src/Vingerspelmenu.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import Navbar from "./components/navbar-mobile.jsx";
 import Hrlogo from "./components/hrlogo.jsx";
+import NavbarMobile from "./components/navbar-mobile.jsx";
 
 function Vingerspelmenu() {
     const navigate = useNavigate();
@@ -36,7 +37,7 @@ function Vingerspelmenu() {
                     </div>
                 </div>
             </div>
-            <Navbar />
+            <NavbarMobile />
         </div>
     );
 }
