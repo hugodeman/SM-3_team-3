e526763c7f1397620106a7c7a0706c98893a3037
justin <kinhanjustin@gmail.com>
2025-03-11 10:56:22 +0100

    hrlogo margin aanpassing

--- src/components/hrlogo.jsx
+++ src/components/hrlogo.jsx
@@ -2,7 +2,6 @@ const HrLogo = () => {
     return (
         <div>
             <img className="absolute right-0 top-0 m-4 max-w-16" src="../src/assets/Logo/HR-Logo-200px.png" alt="HR Logo"/>
-            <div className="pt-10"></div>
         </div>
     );
 }
