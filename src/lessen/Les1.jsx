e2dcc9f2e857a125387f216d321b84d3fa5e227d
miso. <miso.s2000@hotmail.com>
2025-03-11 15:06:45 +0100

    Gramatica les 1

--- src/lessen/Les1.jsx
+++ src/lessen/Les1.jsx
@@ -22,19 +22,19 @@ function Les1() {
             <HrLogo/>
             <div className={'mr-5 ml-5'}>
                 <h1 className={'m-1 text-2xl font-bold '}>Les 1</h1>
-                <h2 className={'font-bold text-xl py-4'}> leer de gebaren </h2>
+                <h2 className={'font-bold text-xl py-4'}> Leer de gebaren </h2>
                 <div className={'pb-5'}>
                     <Buttons text="Bekijk gebaren" to="/lesstof/week1/woordenoverzicht"/>
                 </div>
 
                 <div className={'border-t border-black pt-5'}>
-                    <h2 className={'font-bold text-xl pb-5'}>Oefen de gebaren</h2>
+                    <h2 className={'font-bold text-xl pb-5'}>Oefen de gebaren en de woorden</h2>
                     <div className={'flex justify-between items-center'}>
                         <h3 className={'text-lg mr-10 pb-3 font-bold'}>Opdracht 1</h3>
                         <Buttons text="30/30"/>
                     </div>
                     <div>
-                        <p className={'text-lg py-4'}>oefen woorden</p>
+                        <p className={'text-lg py-4'}>Oefen de woorden naar de gebaren</p>
                     </div>
                     <div className={'pb-5'}>
                         <Buttons text="Oefen" to="/opdracht1"/>
@@ -47,7 +47,7 @@ function Les1() {
                         <Buttons text="30/30"/>
                     </div>
                     <div>
-                        <p className={'text-lg py-4'}>oefen gebaren</p>
+                        <p className={'text-lg py-4'}>Oefen de gebaren naar de woorden</p>
                     </div>
                     <div className={'pb-5'}>
                         <Buttons text="Oefen" to="/opdracht2"/>
@@ -60,7 +60,7 @@ function Les1() {
                         <Buttons text="0/30"/>
                     </div>
                     <div>
-                        <p className={'text-lg py-4'}>oefen zinnen</p>
+                        <p className={'text-lg py-4'}>Oefen zinnen</p>
                     </div>
                     <div className={'pb-5'}>
                         <Buttons text="Oefen" to="/opdracht3"/>
@@ -74,7 +74,7 @@ function Les1() {
                         <Buttons text="0/10"/>
                     </div>
                     <div>
-                        <p className={'text-lg py-4'}>toets </p>
+                        <p className={'text-lg py-4'}>Toets </p>
                     </div>
                     <div className={'pb-5 mb-20'}>
                         <Buttons text="Oefen" to="/opdracht1"/>
