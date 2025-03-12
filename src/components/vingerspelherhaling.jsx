4d1f102c7ae95cec12be98b833d7e731f4d68ebc
justin <kinhanjustin@gmail.com>
2025-03-12 11:32:15 +0100

    Vingerspel herhaling fix

--- src/components/vingerspelherhaling.jsx
+++ src/components/vingerspelherhaling.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import HrlogoPause from "./hrlogo&pause.jsx";
+import { useDarkMode } from "../context/Darkmode.jsx";
 
-function vingerspelherhaling() {
+function Vingerspelherhaling() {
     const navigate = useNavigate();
     const { letter } = useParams();
+    const { darkMode } = useDarkMode();
 
     const handleShowAnswerClick = () => {
         navigate(`/vingerspel/controle/${letter}`);
     };
 
     return (
-        <div className="min-h-screen">
+        <div className={`min-h-screen ${darkMode ? "bg-backgroundDarkMode text-white" : "bg-background text-black"}`}>
             <HrlogoPause/>
             <div className="mt-10">
-                <h1 className="text-3xl font-bold my-2 ml-[5%]">Herhaling: {letter}</h1>
-                <p className="text-base my-2 ml-[5%]">Gebaar de letter in Nederlandse Gebarentaal</p>
+                <h1 className={`text-3xl font-bold my-2 ml-[5%] ${darkMode ? "text-white" : "text-black"}`}>Herhaling: {letter}</h1>
+                <p className={`text-base my-2 ml-[5%] ${darkMode ? "text-gray-300" : "text-gray-800"}`}>Gebaar de letter in Nederlandse Gebarentaal</p>
 
                 <div
-                    className="flex flex-col items-center justify-center w-60 h-80 border-2 border-[#CF0245] bg-white rounded-lg font-bold text-6xl pb-3 mx-auto mt-8">
+                    className={`flex flex-col items-center justify-center w-60 h-80 border-2 rounded-lg font-bold text-6xl pb-3 mx-auto mt-8 ${
+                        darkMode ? "border-gray-600 bg-gray-700" : "border-[#CF0245] bg-white"
+                    }`}>
                     {letter}
                 </div>
 
@@ -34,4 +38,4 @@ function vingerspelherhaling() {
     );
 }
 
-export default vingerspelherhaling;
+export default Vingerspelherhaling;
