645f5f1262fe6fdf33d0a0b0bb54406211ffdf39
hugodeman <1078703@hr.nl>
2025-03-13 13:34:52 +0100

    Fixed navigating

--- src/Lesstof.jsx
+++ src/Lesstof.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import NavbarMobile from "./components/navbar-mobile.jsx";
 import HrLogo from "./components/hrlogo.jsx";
 
 function Lessons() {
+    const { lessonId } = useParams();
+
     const weeks = [
         "Week 1", "Week 2", "Week 3", "Week 4",
         "Week 5", "Week 6", "Week 7"
@@ -16,22 +18,26 @@ function Lessons() {
                 <h1 className="text-3xl font-bold mb-10 ml-8">Lesstof</h1>
             </div>
             <div className="pb-10 mb-10">
-                {Array.from({length: weeks.length}).map((_, i) => (
-                    <div key={i} className="flex justify-around mb-20">
-                        {weeks.slice(i * 2, i * 2 + 2).map((week, j) => (
-                            <div key={j} className="relative">
-                                <Link
-                                    to={`/lesstof/${week.toLowerCase().replace(" ", "")}`}
-                                    className="bg-customRed hover:bg-customRedHover font-bold py-4 px-10 text-white rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl"
-                                >
-                                    {week}
-                                </Link>
+                {Array.from({length: Math.ceil(weeks.length / 2)}).map((_, rowIndex) => (
+                    <div key={rowIndex} className="flex justify-around mb-20">
+                        {weeks.slice(rowIndex * 2, rowIndex * 2 + 2).map((week, columnIndex) => {
+                            const weekNumber = rowIndex * 2 + columnIndex + 1;
 
-                            </div>
-                        ))}
+                            return (
+                                <div key={columnIndex} className="relative">
+                                    <Link
+                                        to={`/les/${weekNumber}`}
+                                        className="bg-customRed hover:bg-customRedHover font-bold py-4 px-10 text-white rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl"
+                                    >
+                                        {week}
+                                    </Link>
+                                </div>
+                            );
+                        })}
                     </div>
                 ))}
             </div>
+
             <NavbarMobile/>
         </>
     );
