e229c2b65ddf0a0a2af72e62d9b1aae9f8915c8d
justin <kinhanjustin@gmail.com>
2025-03-17 15:17:15 +0100

    video zinnen doet het

--- src/opdrachten/ZinnenMaken.jsx
+++ src/opdrachten/ZinnenMaken.jsx
@@ -5,44 +5,60 @@ import Navbar from "../components/navbar-mobile.jsx";
 import HrLogo from "../components/hrlogo.jsx";
 import HrlogoPause from "../components/hrlogo&pause.jsx";
 
-const link = import.meta.env.VITE_GENERAL_LINK;
 const token = import.meta.env.VITE_BEARER_TOKEN;
+const link = import.meta.env.VITE_GENERAL_LINK;
 
 function ZinnenMaken() {
-    const [words, setWords] = useState([]);
+    const [sentences, setSentences] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
     const { lessonId } = useParams();
     const navigate = useNavigate();
     const [sentence, setSentence] = useState("");
 
-    useEffect(() => {
-        fetch(`${link}/words?lesson_id=${lessonId}`, {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json"
+    async function fetchSentences() {
+        try {
+            const res = await fetch(`${link}/sentence-building`, {
+                method: "GET",
+                headers: {
+                    "Accept": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                }
+            });
+
+            if (!res.ok) {
+                throw new Error(`HTTP-fout! status: ${res.status}`);
             }
-        })
-            .then(res => res.json())
-            .then(data => setWords(data))
-            .catch(err => console.error(err));
+
+            const data = await res.json();
+            setSentences(data);
+            console.log(data)
+        } catch (err) {
+            console.error("Fout bij ophalen data:", err);
+        }
+    }
+
+    useEffect(() => {
+        fetchSentences();
     }, [lessonId]);
 
     const handleCheck = () => {
         console.log("Ingevoerde zin:", sentence);
     };
 
+    const currentSentence = sentences[currentIndex];
+
     return (
         <div className="flex flex-col items-center min-h-screen bg-background p-4">
-
             <HrlogoPause />
             <h1 className="text-2xl font-bold my-4">Zinnen Maken</h1>
 
             {/* Video Weergave */}
             <div className="w-full max-w-2xl bg-white rounded-lg shadow-lg overflow-hidden mb-4">
-                <video controls className="w-full">
-                    <source src="/" type="video/mp4" />
-                    Je browser ondersteunt geen video-element.
-                </video>
+
+                    <video  className="w-full" controls
+                        src={currentSentence.video_path}
+                    />
+
             </div>
 
             {/* Input veld */}
