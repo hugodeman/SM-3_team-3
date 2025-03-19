d0e587ec26c7fab3207ce3258e568e27b696f93b
wesle <wesleynederpel43@gmail.com>
2025-03-19 15:38:41 +0100

    logo fix2.0?

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,6 @@
 import { Link, Outlet } from "react-router-dom";
 import { useDarkMode } from './context/Darkmode.jsx';
+import hrLogo from './assets/Logo/HR-Logo-200px.png';
 import { useEffect, useState } from "react";
 
 function Layout() {
@@ -7,7 +8,7 @@ function Layout() {
 
     return (
         <div className={darkMode ? "bg-backgroundDarkMode text-white" : "bg-background"}>
-            <img className="absolute right-0 top-0 m-4 max-w-16" src="/src/assets/Logo/HR-Logo-200px.png" alt="HR Logo"/>
+            <img className="absolute right-0 top-0 m-4 max-w-16" src={hrLogo} alt="HR Logo"/>
             <div className={darkMode ? "bg-backgroundDarkMode pt-20 text-white" : "bg-background pt-20"}>
                 <Outlet />
             </div>
